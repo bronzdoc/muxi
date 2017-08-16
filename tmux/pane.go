d97d0027bd5a33f4a61b04cecf8ba3353f1b040a
bronzdoc <lsagastume1990@gmail.com>
2017-08-15 22:36:48 -0600

    Execute shell commands in panes Use the window as the first pane to avoid having an extra pane

--- tmux/pane.go
+++ tmux/pane.go
@@ -6,9 +6,13 @@ import (
 	"os/exec"
 )
 
+var PANE_INDEX = 0
+
 type Pane struct {
-	command []map[string]interface{}
-	shell   []string
+	index       int
+	command     []map[string]interface{}
+	sessionName string
+	commands    []string
 }
 
 func NewPane() *Pane {
@@ -16,6 +20,7 @@ func NewPane() *Pane {
 }
 
 func (p *Pane) Setup(sessionName string) {
+	p.sessionName = sessionName
 	p.command = []map[string]interface{}{
 		{
 			"cmd": BASECOMMAND,
@@ -26,10 +31,48 @@ func (p *Pane) Setup(sessionName string) {
 	}
 }
 
+// Adds a new command to execute in pane
+func (p *Pane) AddCommand(cmd string) {
+	p.commands = append(p.commands, cmd)
+}
+
 // Creates a new tmux pane
 func (p *Pane) Create() {
+	p.index = PANE_INDEX
+
 	for _, c := range p.command {
 		cmd := exec.Command(c["cmd"].(string), c["args"].([]string)...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Start(); err != nil {
+			fmt.Println(err)
+		}
+
+		if err := cmd.Wait(); err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	WINDOW_INDEX = 0
+
+	p.shell(p.commands)
+
+	PANE_INDEX += 1
+}
+
+func (p *Pane) shell(commands []string) {
+	for _, cmd := range commands {
+		shell := []string{
+			"send-keys",
+			"-t",
+			p.sessionName,
+			cmd,
+			"c-m",
+		}
+
+		cmd := exec.Command("tmux", shell...)
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
--- tmux/windows.go
+++ tmux/windows.go
@@ -13,6 +13,7 @@ type Window struct {
 	name        string
 	panes       []*Pane
 	command     []map[string]interface{}
+	commands    []string
 }
 
 func NewWindow(name string) *Window {
@@ -51,7 +52,7 @@ func (w *Window) Name() string {
 	return w.name
 }
 
-// Creates a new tmux session
+// Creates a new tmux window
 func (w *Window) Create() {
 	for _, c := range w.command {
 		cmd := exec.Command(c["cmd"].(string), c["args"].([]string)...)
@@ -68,12 +69,43 @@ func (w *Window) Create() {
 		}
 	}
 
-	// Create session windows
-	for _, p := range w.panes {
-		p.Create()
+	if len(w.panes) != 0 {
+		firstIndex := 0
+		firstPane := w.panes[firstIndex]
+
+		w.shell(firstPane.commands)
+
+		for _, p := range w.panes[(firstIndex + 1):] {
+			p.Create()
+		}
 	}
 }
 
 func (w *Window) SessionName() string {
 	return w.sessionName
+}
+
+func (w *Window) shell(commands []string) {
+	for _, cmd := range commands {
+		shell := []string{
+			"send-keys",
+			"-t",
+			w.sessionName,
+			cmd,
+			"c-m",
+		}
+
+		cmd := exec.Command("tmux", shell...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Start(); err != nil {
+			fmt.Println(err)
+		}
+
+		if err := cmd.Wait(); err != nil {
+			fmt.Println(err)
+		}
+	}
 }
