d50a7b3c725b3fb224c794272becca32055452f1
bronzdoc <lsagastume1990@gmail.com>
2017-08-22 08:01:49 -0600

    Add main.go

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import "github.com/bronzdoc/muxi/tmux"
+
+func main() {
+	s := tmux.NewSession("test-doc2")
+
+	w := tmux.NewWindow("")
+	s.AddWindow(w)
+
+	w1 := tmux.NewWindow("")
+	s.AddWindow(w1)
+
+	p := tmux.NewPane()
+	p.AddCommand("ls")
+	p2 := tmux.NewPane()
+	p.AddCommand("zshrc")
+	p3 := tmux.NewPane()
+	//p2 := tmux.NewPane()
+	w.AddPane(p)
+	w.AddPane(p2)
+	w.AddPane(p3)
+	//w.AddPane(p2)
+
+	s.Create()
+	//cmd := exec.Command("tmux", "select-window", "-t", "2")
+	//cmd.Stdin = os.Stdin
+	//cmd.Stdout = os.Stdout
+	//cmd.Stderr = os.Stderr
+
+	//out, err := cmd.Output()
+
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+
+	//if err := cmd.Start(); err != nil {
+	//	fmt.Println(err)
+	//}
+
+	//if err := cmd.Wait(); err != nil {
+	//	fmt.Println(err)
+	//}
+
+	//fmt.Println(string(out))
+}
