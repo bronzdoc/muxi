53973b79482519912ba0a5f89169706f6bf58826
bronzdoc <lsagastume1990@gmail.com>
2017-11-06 09:44:52 -0600

    Remove logic from root command

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/bronzdoc/muxi/layout"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -10,37 +9,11 @@ import (
 )
 
 var cfgFile string
-var show string
-var edit string
-var list bool
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "muxi <layout>",
 	Short: "Tmux layout automation",
-
-	Run: func(cmd *cobra.Command, args []string) {
-
-		if list {
-			for _, layoutName := range layout.List() {
-				fmt.Printf("⚫ %s\n", layoutName)
-			}
-		}
-
-		if show != "" {
-			fmt.Println(string(showLayout(show)))
-		}
-
-		if edit != "" {
-			if err := layout.Edit(edit); err != nil {
-				fmt.Println(err)
-			}
-		}
-
-		if len(args) > 0 {
-			createLayout(args[0])
-		}
-	},
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -55,9 +28,6 @@ func Execute() {
 func init() {
 
 	cobra.OnInitialize(initConfig)
-	RootCmd.Flags().StringVarP(&show, "show", "s", "", "show the content of a layout")
-	RootCmd.Flags().StringVarP(&edit, "edit", "e", "", "edit the content of a layout")
-	RootCmd.Flags().BoolVarP(&list, "list", "l", false, "list layouts")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -82,24 +52,4 @@ func initConfig() {
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
-}
-
-func createLayout(layoutName string) {
-	muxi_layout := layout.New(layoutName)
-
-	if err := muxi_layout.Parse(); err != nil {
-		fmt.Println(err)
-	}
-
-	muxi_layout.Create()
-}
-
-func showLayout(layoutName string) []byte {
-	muxi_layout := layout.New(layoutName)
-
-	if err := muxi_layout.Parse(); err != nil {
-		fmt.Println(err)
-	}
-
-	return muxi_layout.RawContent()
 }
